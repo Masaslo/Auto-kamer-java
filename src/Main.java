9dd11a805951b6d43f7090eb46b06305e41ee8bf
Floris <floris.m.boers@gmail.com>
2023-05-22 16:45:05 +0200

    Made starter Mqtt shit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import Server.ServerController;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MqttException {
         System.out.println("Hello world!");
         ServerController serverTest = new ServerController();
     }
--- src/Server/ServerController.java
+++ src/Server/ServerController.java
@@ -1,7 +1,6 @@
 package Server;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.*;
 
 public class ServerController {
 
@@ -9,16 +8,46 @@ public class ServerController {
     public static String MQTT_CLIENT_ID = "javaClass";
     public static String MQTT_USERNAME = "boersfm";
     public static String MQTT_PASSWORD = "P6Kv9Kakc5VcGPBu6FVr";
-    public static String Topic = "boersfm/#";
-    public final String[] Topics = {"boersfm/on", "boersfm/restart/robot"};
+    public static String Topic = "boersfm/thuis";
 
+    MqttClient client;
+    MqttConnectOptions options;
 
-    public ServerController(){
-        MqttClient client;
-        MqttConnectOptions options;
+    public ServerController() throws MqttException {
 
+        client = new MqttClient(MQTT_HOST, MQTT_CLIENT_ID);
+        options = new MqttConnectOptions();
+        options.setUserName(MQTT_USERNAME);
+        options.setPassword(MQTT_PASSWORD.toCharArray());
+        client.connect(options);
 
+        //check if client connected
+        if(client.isConnected()) {
+            System.out.printf("Connect mqtt to: %s\n", MQTT_HOST);
+        }
+        //do things based on the callback
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("connection lost");
+            }
 
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                checkMessage(topic, mqttMessage);
+            }
 
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+
+        client.subscribe(Topic);
+    }
+
+    public void checkMessage(String topic, MqttMessage mqttMessage){
+        String messageString = mqttMessage.toString();
+        System.out.println(messageString);
     }
 }
