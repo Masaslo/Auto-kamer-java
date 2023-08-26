85b5cf05d4a084a5027f53ad761696f70c11c129
Floris <floris.m.boers@gmail.com>
2023-08-26 16:23:22 +0200

    meer maven stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,12 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Main {
     public static void main(String[] args) throws MqttException {
-        System.out.println("Hello world!");
-        ServerController serverTest = new ServerController();
+        System.out.println("initializing MQTT connection");
+        try{
+            ServerController serverTest = new ServerController();
+            System.out.println("initialized");
+        } catch (Exception e){
+            System.out.println("Error:  " + e);
+        }
     }
 }
